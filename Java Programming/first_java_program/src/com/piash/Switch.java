c6ec8e74f3ca6049b46e02e55ca1e2c9672fd0fb
Ashraful-Piash <piashrafulhaque@gmail.com>
2022-03-16 21:37:21 +0600

    Switch program modified

--- Java Programming/first_java_program/src/com/piash/Switch.java
+++ Java Programming/first_java_program/src/com/piash/Switch.java
@@ -9,21 +9,12 @@ public class Switch {
         Scanner in = new Scanner(System.in);
         String fruit = in.next();
 
-        switch (fruit){
-            case "Mango" :
-                System.out.println("King of fruits");
-                break;
-            case "Apple":
-                System.out.println("A sweet red fruit");
-                break;
-            case "Orange":
-                System.out.println("Round fruit");
-                break;
-            case "Jackfruit":
-                System.out.println("National fruit of Bangladesh");
-                break;
-            default:
-                System.out.println("Please enter a valid fruit");
+        switch (fruit) {
+            case "Mango" -> System.out.println("King of fruits");
+            case "Apple" -> System.out.println("A sweet red fruit");
+            case "Orange" -> System.out.println("Round fruit");
+            case "Jackfruit" -> System.out.println("National fruit of Bangladesh");
+            default -> System.out.println("Please enter a valid fruit");
         }
 
     }
