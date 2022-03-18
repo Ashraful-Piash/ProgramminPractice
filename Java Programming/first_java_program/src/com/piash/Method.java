867456599dffc9755e8812dcefa5bc3193089bda
Ashraful-Piash <piashrafulhaque@gmail.com>
2022-03-18 19:57:12 +0600

    New code added

---
+++ Java Programming/first_java_program/src/com/piash/Method.java
@@ -0,0 +1,12 @@
+package com.piash;
+
+public class Method {
+    static void myMethod(String fname) {
+        System.out.println(fname + " Refsnes");
+    }
+    public static void main(String[] args) {
+        myMethod ("Liam");
+        myMethod ("biam");
+        myMethod ("Anja");
+    }
+}
