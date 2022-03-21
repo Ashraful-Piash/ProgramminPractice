4965e146c6204e9d1cf852ece2e2d709f9c322b1
Ashraful-Piash <piashrafulhaque@gmail.com>
2022-03-21 20:54:45 +0600

    Array code added

---
+++ Java Programming/first_java_program/src/com/piash/Array.java
@@ -0,0 +1,15 @@
+package com.piash;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] arr = new int[5];
+        arr[0] = 23;
+        arr[1] = 45;
+        arr[2] = 233;
+        arr [3] = 567;
+        arr [4] = 5;
+        System.out.println(arr[3]);
+
+
+    }
+}
--- Kotlin Programming/Kotlin Program/src/Function.kt
+++ Kotlin Programming/Kotlin Program/src/Function.kt
@@ -1,11 +1,13 @@
-fun main(){
-    printPow(3,5)
+fun main() {
+    printPow(3, 5)
+    printPow(5,3)
+    printPow(7,8)
 
 }
 
-fun printPow (base: Int, exponent: Int){
+fun printPow (base : Int, exponent: Int){
     var result = 1
-    for (i in 1..exponent){
+    for( i in 1..exponent){
         result *= base
     }
     println("$base to the power of $exponent is $result")
