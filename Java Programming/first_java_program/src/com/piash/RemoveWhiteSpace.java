13844c47d4562f46fd70d7f78200a76d6466a4be
Ashraful-Piash <piashrafulhaque@gmail.com>
2022-03-24 20:30:49 +0600

    new code added

--- Java Programming/first_java_program/src/com/piash/RemoveWhiteSpace.java
+++ Java Programming/first_java_program/src/com/piash/RemoveWhiteSpace.java
@@ -1,2 +1,10 @@
-package com.piash;public class RemoveWhiteSpace {
+package com.piash;
+
+public class RemoveWhiteSpace {
+    public static void main(String[] args) {
+        String str = " Java       programming  selenium automation    ";
+
+        str = str.replaceAll("\\s", "");
+        System.out.println("After removing the white spaces: "+str);
+    }
 }
