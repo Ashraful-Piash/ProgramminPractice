70dae1c4fd10c28e2e58fe28a0bdce4bbe55dc52
Ashraful-Piash <piashrafulhaque@gmail.com>
2022-04-06 15:51:22 +0600

    Changed

--- Java Exercise/src/com/piash/Revision.java
+++ Java Exercise/src/com/piash/Revision.java
@@ -10,7 +10,12 @@ public class Revision {
         // primitive type is used for storing simple values while reference type is used for storing storing complex object
         Date now = new Date();
         //now- is an object.object is an instance of class.
-        
+        byte x = 1;
+        byte y =x;
+         x = 2;
+        System.out.println(y);
+
+
         System.out.println(now);
 
         System.out.println();
