99836c6ae5122bd711d1211e433897111fc260dd
Ashraful-Piash <piashrafulhaque@gmail.com>
2022-04-06 15:45:58 +0600

    Code changed

--- Java Exercise/src/com/piash/Revision.java
+++ Java Exercise/src/com/piash/Revision.java
@@ -1,8 +1,20 @@
 package com.piash;
 
+import java.util.Date;
+
 public class Revision {
     public static void main(String[]args){
+
+        int age = 30;
+        //int- variable type, age- identifier, == - assignment operator
+        // primitive type is used for storing simple values while reference type is used for storing storing complex object
+        Date now = new Date();
+        //now- is an object.object is an instance of class.
+        
+        System.out.println(now);
+
         System.out.println();
+
 
     }
 }
